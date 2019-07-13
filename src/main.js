bed4453ab057a9400ce76887b37a9e6fd8b95b44
xiaojun9579 <158665610@qq.com>
2019-07-13 16:29:50 +0800

    前期准备导包

--- src/main.js
+++ src/main.js
@@ -2,7 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// 导入饿了么ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+// 导入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+
+
+// 路由规则
+const routes = [
+  {
+    path:'',
+    component:''
+  }
+]
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
