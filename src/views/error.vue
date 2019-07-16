c5a06fcde0bc5921973061f192611e585f0a3e4d
xiaojun9579 <158665610@qq.com>
2019-07-16 17:13:09 +0800

    地址错误页返回登录页

--- src/views/error.vue
+++ src/views/error.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="my-error">
-        <img src="../assets/404.gif" alt="">
+        <img src="../assets/404.gif" alt="" @click="tologin">
     </div>
 </template>
 
 <script>
 export default {
-name:'error'
+name:'error',
+created() {
+    this.$message.error('哥们你输入的不是正确的地址,点击图片返回登录页')
+},
+methods: {
+    tologin(){
+        // 编程式导航
+        this.$router.push('/login')
+    }
+},
 }
 </script>
 
 <style>
-
+ .my-error{
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+  }
 </style>
